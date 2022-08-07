e6e111ccf1c1d660ee256dd22f976f7eaf62cbde
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2022-08-06 19:32:35 -0500

    Merged HtmlWriterAsync into HtmlWriter and changed it to a readonly struct.

--- HtmlUtilities/HtmlWriter.cs
+++ HtmlUtilities/HtmlWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
 
 namespace HtmlUtilities;
 
@@ -6,7 +7,7 @@ namespace HtmlUtilities;
 /// A high-performance writer for HTML content.
 /// </summary>
 /// <remarks>UTF-8 is always used.</remarks>
-public class HtmlWriter
+public readonly struct HtmlWriter
 {
     private static readonly ValidatedElement html = new(
         new byte[]
@@ -19,7 +20,7 @@ public class HtmlWriter
             (byte)'<', (byte)'/', (byte)'h', (byte)'t', (byte)'m', (byte)'l', (byte)'>',
         });
 
-    private protected readonly IBufferWriter<byte> writer;
+    private readonly IBufferWriter<byte> writer;
 
     internal HtmlWriter(IBufferWriter<byte> writer)
     {
@@ -39,21 +40,22 @@ public class HtmlWriter
     }
 
     /// <summary>
-    /// Writes an HTML document using the provided buffer writer and callbacks.
+    /// Writes an HTML document asynchronously using the provided buffer writer and callbacks.
     /// </summary>
     /// <param name="writer">Receives the written bytes.</param>
     /// <param name="attributes">If provided, writes attributes to the root HTML element.</param>
     /// <param name="children">If provided, writes child elements.</param>
     /// <param name="cancellationToken">Used to trigger cancellation of writing activity.</param>
+    /// <returns>A <see cref="Task"/> to track the progress of the asynchronous operation.</returns>
     /// <exception cref="ArgumentNullException"><paramref name="writer"/> cannot be null.</exception>
     /// <exception cref="OperationCanceledException">A cancellation token in the call tree was triggered.</exception>
     public static Task WriteDocumentAsync(
         IBufferWriter<byte> writer,
         Action<AttributeWriter>? attributes = null,
-        Func<HtmlWriterAsync, CancellationToken, Task>? children = null,
+        Func<HtmlWriter, CancellationToken, Task>? children = null,
         CancellationToken cancellationToken = default)
     {
-        return new HtmlWriterAsync(writer).WriteElementAsync(html, attributes, children, cancellationToken);
+        return new HtmlWriter(writer).WriteElementAsync(html, attributes, children, cancellationToken);
     }
 
     /// <summary>
@@ -103,14 +105,14 @@ public class HtmlWriter
         }
     }
 
-    private protected static void WriteGreaterThan(IBufferWriter<byte> writer)
+    private static void WriteGreaterThan(IBufferWriter<byte> writer)
     {
         var chars = writer.GetSpan();
         chars[0] = (byte)'>';
         writer.Advance(1);
     }
 
-    private protected static void WriteLessThan(IBufferWriter<byte> writer)
+    private static void WriteLessThan(IBufferWriter<byte> writer)
     {
         var chars = writer.GetSpan();
         chars[0] = (byte)'<';
@@ -330,5 +332,125 @@ public class HtmlWriter
         writer.Write(new[] { (byte)'<', (byte)'s', (byte)'c', (byte)'r', (byte)'i', (byte)'p', (byte)'t' });
         writer.Write(script.value);
         writer.Write(new[] { (byte)'<', (byte)'/', (byte)'s', (byte)'c', (byte)'r', (byte)'i', (byte)'p', (byte)'t', (byte)'>' });
+    }
+
+    /// <summary>
+    /// Writes a validated element with optional attributes and child content.
+    /// </summary>
+    /// <param name="element">The validated HTML element.</param>
+    /// <param name="attributes">If provided, writes attributes to the element. Elements baked into the start tag are always included.</param>
+    /// <param name="children">If provided, writes child elements.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that represents the operation.</returns>
+    /// <exception cref="OperationCanceledException">A cancellation token in the call tree was triggered.</exception>
+    /// <exception cref="ArgumentException"><paramref name="element"/> was never initialized.</exception>
+    public async Task WriteElementAsync(
+        ValidatedElement element,
+        Action<AttributeWriter>? attributes = null,
+        Func<HtmlWriter, CancellationToken, Task>? children = null,
+        CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var writer = this.writer;
+        var start = element.start;
+        var end = element.end;
+
+        if (start is null || end is null)
+            throw new ArgumentException("element was never initialized.", nameof(element));
+
+        if (attributes is null)
+            writer.Write(start);
+        else
+        {
+            writer.Write(start.AsSpan(0, start.Length - 1));
+
+            attributes(new AttributeWriter(this.writer));
+
+            WriteGreaterThan(writer);
+        }
+
+        if (children is not null)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            await children(this, cancellationToken).ConfigureAwait(false);
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        writer.Write(end);
+    }
+
+    /// <summary>
+    /// Writes an element with optional attributes and child content.
+    /// If there are no children (for example, <paramref name="children"/> is null or writes nothing), use <see cref="WriteElement(ReadOnlySpan{char}, Action{AttributeWriter}?, Action{HtmlWriter}?)"/> instead.
+    /// </summary>
+    /// <param name="name">The HTML element name.</param>
+    /// <param name="attributes">If provided, writes attributes to the element. Elements baked into the start tag are always included.</param>
+    /// <param name="children">
+    /// Writes child elements.
+    /// This parameter is technically optional to provide symmetry with <see cref="WriteElement(ReadOnlySpan{char}, Action{AttributeWriter}?, Action{HtmlWriter}?)"/>.
+    /// However, this method should not be used without children--it will run synchronously by forwarding to the non-async version of this API.
+    /// </param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that represents the operation.</returns>
+    /// <exception cref="OperationCanceledException">A cancellation token in the call tree was triggered.</exception>
+    /// <exception cref="ArgumentException">The element name is not valid.</exception>
+    public Task WriteElementAsync(
+        ReadOnlySpan<char> name,
+        Action<AttributeWriter>? attributes = null,
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        [DisallowNull] Func<HtmlWriter, CancellationToken, Task>? children = null,
+#pragma warning restore
+        CancellationToken cancellationToken = default)
+    {
+        if (children is null) // DisallowNull discourages people from using this API this way, but we'll still support it.
+        {
+            this.WriteElement(name, attributes, null);
+            return Task.CompletedTask;
+        }
+
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var writer = this.writer;
+
+        var elementNameWriter = new ArrayBuilder<byte>(name.Length);
+
+        ValidatedElementName.Validate(name, ref elementNameWriter);
+        var elementNameBuffer = elementNameWriter.Buffer;
+        var validatedName = elementNameWriter.WrittenMemory;
+
+        WriteLessThan(writer);
+        writer.Write(validatedName.Span);
+
+        if (attributes is not null)
+            attributes(new AttributeWriter(writer));
+
+        WriteGreaterThan(writer);
+
+        static async Task WriteElementAsync(
+            HtmlWriter htmlWriterAsync,
+            ReadOnlyMemory<byte> validatedName,
+            byte[] elementNameBuffer,
+            Func<HtmlWriter, CancellationToken, Task> children,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                await children(htmlWriterAsync, cancellationToken).ConfigureAwait(false);
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var writer = htmlWriterAsync.writer;
+                writer.Write(new[] { (byte)'<', (byte)'/' });
+                writer.Write(validatedName.Span);
+                WriteGreaterThan(writer);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(elementNameBuffer);
+            }
+        }
+
+        return WriteElementAsync(this, validatedName, elementNameBuffer, children, cancellationToken);
     }
 }
