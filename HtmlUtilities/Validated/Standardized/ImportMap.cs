64fffe2a4418e7fbf49b686164dfb27a2c1c0960
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2024-08-24 08:28:51 -0500

    Added a helper class for import maps.

---
+++ HtmlUtilities/Validated/Standardized/ImportMap.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HtmlUtilities.Validated.Standardized;
+
+/// <summary>
+/// Contents for a specialized "script" element that allows developers to control how the browser resolves module specifiers when importing JavaScript modules.
+/// Reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type/importmap
+/// </summary>
+public sealed class ImportMap
+{
+    /// <summary>
+    /// Replaces module specifiers in matching import statements.
+    /// Effects can be overridden by a match within <see cref="Scopes"/>.
+    /// </summary>
+    [JsonPropertyName("imports")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public IDictionary<string, string>? Imports { get; set; }
+
+    /// <summary>
+    /// Provides integrity metadata for module sources.
+    /// </summary>
+    [JsonPropertyName("integrity")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public IDictionary<string, string>? Integrity { get; set; }
+
+    /// <summary>
+    /// Path-specific module specifier maps.
+    /// </summary>
+    [JsonPropertyName("scopes")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public IDictionary<string, IDictionary<string, string>>? Scopes { get; set; }
+
+    /// <inheritdoc />
+    public override string ToString() => JsonSerializer.Serialize(this, ImportMapSerializerContext.Default.ImportMap);
+}
+
+[JsonSerializable(typeof(ImportMap))]
+internal sealed partial class ImportMapSerializerContext : JsonSerializerContext
+{
+}
